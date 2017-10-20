5624fff28eec9b9e43096e3d05278248faf27b22
SatDevArise <prj.sat.dev@arise.co.jp>
2017-10-20 13:51:40 +0900

    山本　真士 2017年10月20日                        　　 　　　　　　　　　　　　　　　　　　　　　　　　　　 対応名：15期土曜勉強会_大阪　　　　　　　　　　　　 コメント：formの編集　

--- src/main/java/jp/arise/sij/form/SIJGM002Form.java
+++ src/main/java/jp/arise/sij/form/SIJGM002Form.java
@@ -12,6 +12,24 @@ public class SIJGM002Form {
 	 * ユーザー
 	 */
 	private String user;
+	private String syain_id;
+	private String name;
+	private String birth;
+	private String moyori_1_station;
+	private String moyori_2_station;
+	private String moyori_3_station;
+	private String area_cd;
+	private String city_cd;
+	private String subscriber_no;
+	private String previous;
+	private String brth_place;
+	private String psition;
+	private String team_na;
+	private String phase_kb;
+	private String tanka;
+	private String genba_na;
+	private String kensakuKeyList;
+	private String page;
 
 	/**
 	 * ユーザーを取得
@@ -20,7 +38,13 @@ public class SIJGM002Form {
 	public String getUser() {
 		return user;
 	}
-
+	/**
+	 * 社員IDを取得
+	 * @return user
+	 */
+	public String getsyain_cd() {
+		return syain_cd;
+	}
 	/**
 	 * ユーザーを設定
 	 * @param user
@@ -28,5 +52,11 @@ public class SIJGM002Form {
 	public void setUser(String user) {
 		this.user = user;
 	}
-
+	/**
+	 * 社員IDを設定
+	 * @param user
+	 */
+	public void setsyain_cd(String syain_cd) {
+		this.syain_cd = syain_cd;
+	}
 }
