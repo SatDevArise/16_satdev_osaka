eb9a42028d0343c4faec0cc517c850ef48e97a90
SatDevArise <prj.sat.dev@arise.co.jp>
2018-02-24 14:06:17 +0900

    山本真士　2018年2月24日
    対応名：15期土曜勉強会_大阪
    コメント：Serviseファイルを編集

--- src/main/java/jp/arise/sij/service/SIJGM002Servise.java
+++ src/main/java/jp/arise/sij/service/SIJGM002Servise.java
@@ -1,16 +1,20 @@
 package jp.arise.sij.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
+import jp.arise.com.message.COMMessage;
 import jp.arise.sij.dao.SIJGM002Dao;
 import jp.arise.sij.dto.SIJGM002Dto;
-import jp.arise.sij.message.SIJMessage;
+import jp.arise.utl.UTLContent;
 
 /**
  * SIJGM002 社員情報新規登録・編集画面用サービス
+ *
  * @author AtsushiNishizawa
  * @since 2017/07/17
  */
@@ -22,34 +26,139 @@ public class SIJGM002Servise {
 
 	/**
 	 * 社員ID取得処理
+	 *
 	 * @param dto
 	 * @return
 	 */
 	public SIJGM002Dto getSyain_dt(SIJGM002Dto dto) {
 		String syain_id = dto.getSyain_id();
 		System.out.println(syain_id);
-		
+
 		return dto;
 	}
 
-
 	/**
-	 * 入力チェック（サンプル）
+	 * 入力チェック
+	 *
 	 * @param dto
 	 * @return
 	 */
 	public SIJGM002Dto inputCheck(SIJGM002Dto dto) {
-        String user = dto.getUser();
-        System.out.println(user);
 
-        dto.setUserId(2);
+		List<String> resultMessage = new ArrayList<String>();
+		// 社員ID：必須入力チェック
+		if (StringUtils.isEmpty(dto.getSyain_id())) {
+			// resultMessage.add(COMMessage.COME005.getMessage());
+		} else {
+			// 社員ID：半角文字チェック
+			if (!patternCheck(dto.getSyain_id())) {
+				resultMessage.add(COMMessage.COME001.getMessage());
+			}
+			// 社員ID：桁数チェック
+			if (!digitCheck(dto.getSyain_id(), UTLContent.INT_EIGHT)) {
+				resultMessage.add(COMMessage.COME002.getMessage());
+			}
+		}
 
-        List<SIJGM002Dto> resultList = sijGm002Dao.getUser(dto);
+		// 氏名：必須入力チェック
+		if (StringUtils.isEmpty(dto.getName())) {
+			// resultMessage.add(COMMessage.COME005.getMessage());
+		} else {
+			// 氏名：全角文字チェック
+			if (patternCheck(dto.getName())) {
+				resultMessage.add(COMMessage.COME001.getMessage());
+			}
+			// 氏名：桁数チェック
+			if (!digitCheck(dto.getName(), UTLContent.INT_EIGHT)) {
+				resultMessage.add(COMMessage.COME002.getMessage());
+			}
+		}
 
-        System.out.println(resultList.get(0).getUser());
+		// 生年月日：必須入力チェック
+		if (StringUtils.isEmpty(dto.getBirth())) {
+			// resultMessage.add(COMMessage.COME005.getMessage());
+		} else {
+			// 生年月日：半角文字チェック
+			if (!patternCheck(dto.getBirth())) {
+				resultMessage.add(COMMessage.COME001.getMessage());
+			}
+			// 生年月日：桁数チェック
+			if (!digitCheck(dto.getBirth(), UTLContent.INT_EIGHT)) {
+				resultMessage.add(COMMessage.COME002.getMessage());
+			}
+		}
+		// 最寄り駅1：必須入力チェック
+		if (StringUtils.isEmpty(dto.getMoyori_1_station())) {
+			// resultMessage.add(COMMessage.COME005.getMessage());
+		} else {
+			// 最寄り駅1：全角文字チェック
+			if (patternCheck(dto.getMoyori_1_station())) {
+				resultMessage.add(COMMessage.COME001.getMessage());
+			}
+			// 最寄り駅1：桁数チェック
+			if (!digitCheck(dto.getMoyori_1_station(), UTLContent.INT_EIGHT)) {
+				resultMessage.add(COMMessage.COME002.getMessage());
+			}
+		}
 
-        System.out.println(SIJMessage.SIJE001.getMessage());
+		// 最寄り駅2：必須入力チェック
+		if (!StringUtils.isEmpty(dto.getMoyori_3_station())) {
+			// resultMessage.add(COMMessage.COME005.getMessage());
+		} else {
+			// 最寄り駅2：全角文字チェック
+			if (patternCheck(dto.getMoyori_2_station())) {
+				resultMessage.add(COMMessage.COME001.getMessage());
+			}
+			// 最寄り駅2：桁数チェック
+			if (!digitCheck(dto.getMoyori_2_station(), UTLContent.INT_EIGHT)) {
+				resultMessage.add(COMMessage.COME002.getMessage());
+			}
+		}
+		// 連絡先：必須入力チェック
+		if (!StringUtils.isEmpty(dto.getPhoneNumber())) {
+			// resultMessage.add(COMMessage.COME005.getMessage());
+		} else {
+			// 連絡先：半角文字チェック
+			if (!patternCheck(dto.getPhoneNumber())) {
+				resultMessage.add(COMMessage.COME001.getMessage());
+			}
+			// 連絡先：桁数チェック
+			if (!digitCheck(dto.getPhoneNumber(), UTLContent.INT_EIGHT)) {
+				resultMessage.add(COMMessage.COME002.getMessage());
+			}
+		}
 
 		return dto;
 	}
+
+	/**
+	 * 半角文字チェック
+	 *
+	 * @param String
+	 * @return true:OK false:NG
+	 * @suthor Masashi.Yamamoto
+	 */
+	private boolean patternCheck(String str) {
+		if (!str.matches("^[0-9A-Za-z]+$")) {
+			return false;
+		}
+		return true;
+	}
+
+
+
+	/**
+	 * 桁数チェック
+	 *
+	 * @param String,int
+	 * @return true:OK false:NG
+	 * @author MasashiYamamoto
+	 */
+	private boolean digitCheck(String str, int digit) {
+		if (!(digit == str.length())) {
+			return false;
+		}
+		return true;
+	}
+
 }
