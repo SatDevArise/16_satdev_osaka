824c737d7787fe6a05fde33be94aa8d578e842ea
SatDevArise <prj.sat.dev@arise.co.jp>
2018-03-06 00:31:08 +0900

    森　翔一　2018年3月6日
    対応名：15期土曜勉強会_大阪
    コメント：サービスクラスの修正

--- src/main/java/jp/arise/sij/service/SIJGM002Servise.java
+++ src/main/java/jp/arise/sij/service/SIJGM002Servise.java
@@ -7,9 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
-import jp.arise.com.message.COMMessage;
 import jp.arise.sij.dao.SIJGM002Dao;
 import jp.arise.sij.dto.SIJGM002Dto;
+import jp.arise.sij.message.SIJMessage;
 import jp.arise.utl.UTLContent;
 
 /**
@@ -68,83 +68,83 @@ public class SIJGM002Servise {
 		List<String> resultMessage = new ArrayList<String>();
 		// 社員ID：必須入力チェック
 		if (StringUtils.isEmpty(dto.getSyain_id())) {
-			// resultMessage.add(COMMessage.COME005.getMessage());
+			// resultMessage.add(SIJMessage.COME005.getMessage());
 		} else {
 			// 社員ID：半角文字チェック
 			if (!patternCheck(dto.getSyain_id())) {
-				resultMessage.add(COMMessage.COME001.getMessage());
+				resultMessage.add(SIJMessage.SIJE001.getMessage());
 			}
 			// 社員ID：桁数チェック
 			if (!digitCheck(dto.getSyain_id(), UTLContent.INT_EIGHT)) {
-				resultMessage.add(COMMessage.COME002.getMessage());
+				resultMessage.add(SIJMessage.SIJE002.getMessage());
 			}
 		}
 
 		// 氏名：必須入力チェック
 		if (StringUtils.isEmpty(dto.getName())) {
-			// resultMessage.add(COMMessage.COME005.getMessage());
+			// resultMessage.add(SIJMessage.COME005.getMessage());
 		} else {
 			// 氏名：全角文字チェック
 			if (patternCheck(dto.getName())) {
-				resultMessage.add(COMMessage.COME001.getMessage());
+				resultMessage.add(SIJMessage.SIJE001.getMessage());
 			}
 			// 氏名：桁数チェック
 			if (!digitCheck(dto.getName(), UTLContent.INT_EIGHT)) {
-				resultMessage.add(COMMessage.COME002.getMessage());
+				resultMessage.add(SIJMessage.SIJE002.getMessage());
 			}
 		}
 
 		// 生年月日：必須入力チェック
 		if (StringUtils.isEmpty(dto.getBirth())) {
-			// resultMessage.add(COMMessage.COME005.getMessage());
+			// resultMessage.add(SIJMessage.COME005.getMessage());
 		} else {
 			// 生年月日：半角文字チェック
 			if (!patternCheck(dto.getBirth())) {
-				resultMessage.add(COMMessage.COME001.getMessage());
+				resultMessage.add(SIJMessage.SIJE001.getMessage());
 			}
 			// 生年月日：桁数チェック
 			if (!digitCheck(dto.getBirth(), UTLContent.INT_EIGHT)) {
-				resultMessage.add(COMMessage.COME002.getMessage());
+				resultMessage.add(SIJMessage.SIJE002.getMessage());
 			}
 		}
 		// 最寄り駅1：必須入力チェック
 		if (StringUtils.isEmpty(dto.getMoyori_1_station())) {
-			// resultMessage.add(COMMessage.COME005.getMessage());
+			// resultMessage.add(SIJMessage.COME005.getMessage());
 		} else {
 			// 最寄り駅1：全角文字チェック
 			if (patternCheck(dto.getMoyori_1_station())) {
-				resultMessage.add(COMMessage.COME001.getMessage());
+				resultMessage.add(SIJMessage.SIJE001.getMessage());
 			}
 			// 最寄り駅1：桁数チェック
 			if (!digitCheck(dto.getMoyori_1_station(), UTLContent.INT_EIGHT)) {
-				resultMessage.add(COMMessage.COME002.getMessage());
+				resultMessage.add(SIJMessage.SIJE002.getMessage());
 			}
 		}
 
 		// 最寄り駅2：必須入力チェック
 		if (!StringUtils.isEmpty(dto.getMoyori_3_station())) {
-			// resultMessage.add(COMMessage.COME005.getMessage());
+			// resultMessage.add(SIJMessage.COME005.getMessage());
 		} else {
 			// 最寄り駅2：全角文字チェック
 			if (patternCheck(dto.getMoyori_2_station())) {
-				resultMessage.add(COMMessage.COME001.getMessage());
+				resultMessage.add(SIJMessage.SIJE001.getMessage());
 			}
 			// 最寄り駅2：桁数チェック
 			if (!digitCheck(dto.getMoyori_2_station(), UTLContent.INT_EIGHT)) {
-				resultMessage.add(COMMessage.COME002.getMessage());
+				resultMessage.add(SIJMessage.SIJE002.getMessage());
 			}
 		}
 		// 連絡先：必須入力チェック
 		if (!StringUtils.isEmpty(dto.getPhoneNumber())) {
-			// resultMessage.add(COMMessage.COME005.getMessage());
+			// resultMessage.add(SIJMessage.COME005.getMessage());
 		} else {
 			// 連絡先：半角文字チェック
 			if (!patternCheck(dto.getPhoneNumber())) {
-				resultMessage.add(COMMessage.COME001.getMessage());
+				resultMessage.add(SIJMessage.SIJE001.getMessage());
 			}
 			// 連絡先：桁数チェック
 			if (!digitCheck(dto.getPhoneNumber(), UTLContent.INT_EIGHT)) {
-				resultMessage.add(COMMessage.COME002.getMessage());
+				resultMessage.add(SIJMessage.SIJE002.getMessage());
 			}
 		}
 
