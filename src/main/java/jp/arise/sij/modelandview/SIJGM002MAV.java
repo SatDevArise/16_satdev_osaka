5750eff2590c0e6fac2912082ef42a34c3311230
SatDevArise <prj.sat.dev@arise.co.jp>
2018-07-01 17:32:10 +0900

    森 2018/07/01
    対応名：15期勉強会_大阪
    コメント：MAV項目の見直し

--- src/main/java/jp/arise/sij/modelandview/SIJGM002MAV.java
+++ src/main/java/jp/arise/sij/modelandview/SIJGM002MAV.java
@@ -23,50 +23,50 @@ public class SIJGM002MAV {
 	/**
 	 * 名前
 	 */
-	private String name;
+	private String syain_na;
 
 	/**
 	 * 生年月日
 	 */
-	private String birth;
+	private String birth_dt;
 
 	/**
 	 * 最寄り駅1
 	 *
 	 */
-	private String moyori_1_station;
+	private String moyori_eki_1;
 
 	/**
 	 * 最寄り駅2
 	 *
 	 */
-	private String moyori_2_station;
+	private String moyori_eki_2;
 
 	/**
 	 * 最寄り駅3
 	 *
 	 */
-	private String moyori_3_station;
+	private String moyori_eki_3;
 
 	/**
 	 * 連絡先
 	 */
-	private String phoneNumber;
+	private String syain_renrakusaki;
 
 	/**
 	 * 前職
 	 */
-	private String previous;
+	private String zensyoku_na;
 
 	/**
 	 * 出身地
 	 */
-	private String birth_place;
+	private String syussin_na;
 
 	/**
 	 * 役職
 	 */
-	private String position;
+	private String yakusyoku_cd;
 
 	/**
 	 * 入社日
@@ -76,7 +76,7 @@ public class SIJGM002MAV {
 	/**
 	 * チーム名
 	 */
-	private String team_na;
+	private String syozoku_team;
 
 	/**
 	 * フェーズ
@@ -86,7 +86,7 @@ public class SIJGM002MAV {
 	/**
 	 * 単価
 	 */
-	private String tanka;
+	private String tanka_vl;
 
 	/**
 	 * 現場名
@@ -96,7 +96,7 @@ public class SIJGM002MAV {
 	/**
 	 * 参画日
 	 */
-	private String sankaku_dt;
+	private String genba_sankaku_dt;
 
 	/**
 	 * 検索キーリスト
@@ -135,72 +135,72 @@ public class SIJGM002MAV {
 	 * 名前を取得
 	 * @return name
 	 */
-	public String getName() {
-		return name;
+	public String getSyain_na() {
+		return syain_na;
 	}
 
 	/**
 	 * 生年月日を取得
 	 * @return birth
 	 */
-	public String getBirth() {
-		return birth;
+	public String getBirth_dt() {
+		return birth_dt;
 	}
 
 	/**
 	 * 最寄り駅1を取得
 	 * @return  moyori_1_station
 	 */
-	public String getMoyori_1_station() {
-		return moyori_1_station;
+	public String getMoyori_eki_1() {
+		return moyori_eki_1;
 	}
 
 	/**
 	 * 最寄り駅2を取得
 	 * @return  moyori_2_station
 	 */
-	public String getMoyori_2_station() {
-		return moyori_2_station;
+	public String getMoyori_eki_2() {
+		return moyori_eki_2;
 	}
 
 	/**
 	 * 最寄り駅3を取得
 	 * @return  moyori_3_station
 	 */
-	public String getMoyori_3_station() {
-		return moyori_3_station;
+	public String getMoyori_eki_3() {
+		return moyori_eki_3;
 	}
 
 	/**
 	 * 連絡先を取得
 	 * @return  phoneNumber
 	 */
-	public String getPhoneNumber() {
-		return phoneNumber;
+	public String getSyain_renrakusaki() {
+		return syain_renrakusaki;
 	}
 
 	/**
 	 * 前職を取得
 	 * @return  previous
 	 */
-	public String getPrevious() {
-		return previous;
+	public String getZensyoku_na() {
+		return zensyoku_na;
 	}
 
 	/**
 	 * 出身地
 	 * @return  birth_place
 	 */
-	public String getBirth_place() {
-		return birth_place;
+	public String getSyussin_na() {
+		return syussin_na;
 	}
 
 	/**
 	 * 役職
 	 * @return  position
 	 */
-	public String getPosition() {
-		return position;
+	public String getYakusyoku_cd() {
+		return yakusyoku_cd;
 	}
 
 	/**
@@ -215,8 +215,8 @@ public class SIJGM002MAV {
 	 * チーム名
 	 * @return  team_na
 	 */
-	public String getTeam_na() {
-		return team_na;
+	public String getSyozoku_team() {
+		return syozoku_team;
 	}
 
 	/**
@@ -231,8 +231,8 @@ public class SIJGM002MAV {
 	 * 単価
 	 * @return tanka
 	 */
-	public String getTanka() {
-		return tanka;
+	public String getTanka_vl() {
+		return tanka_vl;
 	}
 
 	/**
@@ -247,8 +247,8 @@ public class SIJGM002MAV {
 	 * 参画日
 	 * @return sankaku_dt
 	 */
-	public String getSankaku_dt() {
-		return sankaku_dt;
+	public String getGenba_sankaku_dt() {
+		return genba_sankaku_dt;
 	}
 
 	/**
@@ -296,72 +296,72 @@ public class SIJGM002MAV {
 	 * 名前を設定
 	 * @param namae
 	 */
-	public void setName(String name) {
-		this.name = name;
+	public void setSyain_na(String syain_na) {
+		this.syain_na = syain_na;
 	}
 
 	/**
 	 * 生年月日を設定
 	 * @param birth
 	 */
-	public void setBirth(String birth) {
-		this.birth = birth;
+	public void setBirth_dt(String birth_dt) {
+		this.birth_dt = birth_dt;
 	}
 
 	/**
 	 * 最寄り駅1を設定
 	 * @param moyori_1_station
 	 */
-	public void setMoyori_1_station(String moyori_1_station) {
-		this.moyori_1_station = moyori_1_station;
+	public void setMoyori_eki_1(String moyori_eki_1) {
+		this.moyori_eki_1 = moyori_eki_1;
 	}
 
 	/**
 	 * 最寄り駅2を設定
 	 * @param moyori_2_station
 	 */
-	public void setMoyori_2_station(String moyori_2_station) {
-		this.moyori_2_station = moyori_2_station;
+	public void setmoyori_eki_2(String moyori_eki_2) {
+		this.moyori_eki_2 = moyori_eki_2;
 	}
 
 	/**
 	 * 最寄り駅3を設定
 	 * @param moyori_3_station
 	 */
-	public void setMoyori_3_station(String moyori_3_station) {
-		this.moyori_3_station = moyori_3_station;
+	public void setmoyori_eki_3(String moyori_eki_3) {
+		this.moyori_eki_3 = moyori_eki_3;
 	}
 
 	/**
 	 * 連絡先を設定
 	 * @param phoneNumber
 	 */
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
+	public void setSyain_renrakusaki(String syain_renrakusaki) {
+		this.syain_renrakusaki = syain_renrakusaki;
 	}
 
 	/**
 	 * 前職を設定
 	 * @param previous
 	 */
-	public void setPrevious(String previous) {
-		this.previous = previous;
+	public void setPrevious(String syain_renrakusaki) {
+		this.syain_renrakusaki = syain_renrakusaki;
 	}
 
 	/**
 	 * 出身地を設定
 	 * @param birth_place
 	 */
-	public void setBirth_place(String birth_place) {
-		this.birth_place = birth_place;
+	public void setSyussin_na(String syussin_na) {
+		this.syussin_na = syussin_na;
 	}
 
 	/**
 	 * 役職を設定
 	 * @param position
 	 */
-	public void setPosition(String position) {
-		this.position = position;
+	public void setYakusyoku_cd(String yakusyoku_cd) {
+		this.yakusyoku_cd = yakusyoku_cd;
 	}
 
 	/**
@@ -376,8 +376,8 @@ public class SIJGM002MAV {
 	 * チーム名を設定
 	 * @param team_na
 	 */
-	public void setTeam_na(String team_na) {
-		this.team_na = team_na;
+	public void setSyozoku_team(String syozoku_team) {
+		this.syozoku_team = syozoku_team;
 	}
 
 	/**
@@ -392,8 +392,8 @@ public class SIJGM002MAV {
 	 * 単価を設定
 	 * @param tanka
 	 */
-	public void setTanka(String tanka) {
-		this.tanka = tanka;
+	public void setTanka_vl(String tanka_vl) {
+		this.tanka_vl = tanka_vl;
 	}
 
 	/**
@@ -408,8 +408,8 @@ public class SIJGM002MAV {
 	 * 参画日を設定
 	 * @param sankaku_dt
 	 */
-	public void setSankaku_dt(String sankaku_dt) {
-		this.sankaku_dt = sankaku_dt;
+	public void setGenba_sankaku_dt(String genba_sankaku_dt) {
+		this.genba_sankaku_dt = genba_sankaku_dt;
 	}
 
 	/**
