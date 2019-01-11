bd35fa99830b6d31a655bc385084fd273e60e002
SatDevArise <prj.dat.dev@arise.co.jp>
2019-01-12 03:31:31 +0900

    Initial Commit

--- src/main/java/jp/arise/com/dao/COMGM001Dao.java
+++ src/main/java/jp/arise/com/dao/COMGM001Dao.java
@@ -1,40 +1,190 @@
 package jp.arise.com.dao;
 
-import java.io.Serializable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.stereotype.Repository;
 
 import jp.arise.com.dto.COMGM001Dto;
 import jp.arise.utl.LoginInfoDto;
 
 /**
- * COMGM001 ログイン画面用サービス間Daoクラス
- * データベースアクセス時に利用するクラス
+ * COMGM001 ログイン画面用サービス間Daoクラス データベースアクセス時に利用するクラス
+ *
  * @author AtsushiNishizawa
  * @since 2017/07/17
  */
-public interface COMGM001Dao extends Serializable{
+@Repository
+public class COMGM001Dao {
 
-	/**
+	@Autowired
+	private NamedParameterJdbcTemplate jdbcTemplate;
+
+	 /**
 	 * ログインユーザー情報取得
 	 * @return LoginInfoDto
 	 */
-	public LoginInfoDto getLoginInfo(COMGM001Dto COMGM001Dto);
+	public LoginInfoDto getLoginInfo(COMGM001Dto COMGM001Dto) {
 
-	/**
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("SELECT");
+		sb.append("  user_id");
+		sb.append("  , user_na");
+		sb.append("  , TO_CHAR(CURRENT_DATE, 'YYYYMMDD') AS unyoubi ");
+		sb.append("FROM");
+		sb.append("  login_user_dt ");
+		sb.append("WHERE");
+		sb.append("  user_id = :userId ");
+		sb.append("  AND password = :password ");
+		sb.append("  AND yuko_fg = '1'");
+
+		SqlParameterSource param = new MapSqlParameterSource()
+				.addValue("userId", COMGM001Dto.getUser_id())
+				.addValue("password", COMGM001Dto.getPassword());
+
+		try {
+
+			return jdbcTemplate.queryForObject(sb.toString(),
+					param,
+					new BeanPropertyRowMapper<LoginInfoDto>(LoginInfoDto.class));
+
+		} catch (EmptyResultDataAccessException e) {
+
+			return null;
+
+		}
+	}
+
+	 /**
 	 * ログイン履歴存在チェック
 	 * @return -
 	 */
-	public String getRireki(LoginInfoDto loginInfoDto);
+	public String getRireki(LoginInfoDto loginInfoDto) {
 
-	/**
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("SELECT");
+		sb.append("  user_id");
+		sb.append("FROM");
+		sb.append("  login_rireki_dt ");
+		sb.append("WHERE");
+		sb.append("  user_id = :userId ");
+		sb.append("  AND yuko_fg = '1'");
+
+		SqlParameterSource param = new MapSqlParameterSource()
+				.addValue("userId", loginInfoDto.getUser_id());
+
+		try {
+
+			String result = jdbcTemplate.queryForObject(sb.toString(),
+					param,String.class);
+
+			return result;
+
+		} catch (EmptyResultDataAccessException e) {
+
+			return null;
+
+		}
+
+	}
+
+	 /**
 	 * ログイン履歴作成処理
 	 * @return -
 	 */
-	public void createRireki(LoginInfoDto loginInfoDto);
+	public int createRireki(LoginInfoDto loginInfoDto) {
 
-	/**
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("INSERT ");
+		sb.append("INTO login_rireki_dt ");
+		sb.append("VALUES ( ");
+		sb.append("  :userId");
+		sb.append("  , :userNa");
+		sb.append("  , TO_CHAR(CURRENT_DATE, 'YYYYMMDD')");
+		sb.append("  , ''");
+		sb.append("  , '1'");
+		sb.append("  , '1'");
+		sb.append("  , :userId");
+		sb.append("  , TO_CHAR(CURRENT_DATE, 'YYYYMMDD')");
+		sb.append("  , ''");
+		sb.append("  , ''");
+		sb.append("  , ''");
+		sb.append("  , ''");
+		sb.append(") ");
+
+		SqlParameterSource param = new MapSqlParameterSource()
+				.addValue("userId", loginInfoDto.getUser_id())
+				.addValue("userNa", loginInfoDto.getUser_na());
+
+		try {
+
+			return jdbcTemplate.update(sb.toString(),param);
+
+		} catch (EmptyResultDataAccessException e) {
+
+			return 0;
+
+		}
+	}
+
+	 /**
 	 * ログイン履歴更新処理
 	 * @return -
 	 */
-	public void updateRireki(LoginInfoDto loginInfoDto);
+	public int updateRireki(LoginInfoDto loginInfoDto) {
+
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("UPDATE login_rireki_dt ");
+		sb.append("SET");
+		sb.append("  login_date = TO_CHAR(CURRENT_DATE, 'YYYYMMDD')");
+		sb.append("  , update_user = :userId");
+		sb.append("  , update_ts = TO_CHAR(CURRENT_DATE, 'YYYYMMDD') ");
+		sb.append("WHERE");
+		sb.append("  user_id = :userId ");
+		sb.append("  AND yuko_fg = '1'");
+
+		SqlParameterSource param = new MapSqlParameterSource()
+				.addValue("userId", loginInfoDto.getUser_id());
+
+		try {
+
+			return jdbcTemplate.update(sb.toString(),param);
+
+		} catch (EmptyResultDataAccessException e) {
+
+			return 0;
+
+		}
+	}
+	// /**
+	// * ログインユーザー情報取得
+	// * @return LoginInfoDto
+	// */
+	// public LoginInfoDto getLoginInfo(COMGM001Dto COMGM001Dto);
+	//
+	// /**
+	// * ログイン履歴存在チェック
+	// * @return -
+	// */
+	// public String getRireki(LoginInfoDto loginInfoDto);
+	//
+	// /**
+	// * ログイン履歴作成処理
+	// * @return -
+	// */
+	// public void createRireki(LoginInfoDto loginInfoDto);
+	//
+	// /**
+	// * ログイン履歴更新処理
+	// * @return -
+	// */
+	// public void updateRireki(LoginInfoDto loginInfoDto);
 
 }
--- src/main/java/jp/arise/sij/dao/SIJGM001Dao.java
+++ src/main/java/jp/arise/sij/dao/SIJGM001Dao.java
@@ -1,28 +1,91 @@
 package jp.arise.sij.dao;
 
-import java.io.Serializable;
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
 
 import jp.arise.sij.dto.SIJGM001Dto;
 
 /**
- * SIJGM001 社員情報一覧表示画面用サービス間Daoクラス
- * データベースアクセス時に利用するクラス
+ * SIJGM001 社員情報一覧表示画面用サービス間Daoクラス データベースアクセス時に利用するクラス
+ *
  * @author AtsushiNishizawa
  * @since 2017/07/17
  */
-public interface SIJGM001Dao extends Serializable{
 
-	/**
-	 * userを取得
-	 * @return user
-	 */
-	public List<SIJGM001Dto> getUser(SIJGM001Dto SIJGM001Dto);
+@Repository
+public class SIJGM001Dao{
 
-	/**
-	 * 社員情報一覧を取得
-	 * @param SIJGM001Dto
-	 * @return getSyainListInfo
-	 */
-	public List<SIJGM001Dto> getSyainListInfo(SIJGM001Dto SIJGM001Dto);
+	@Autowired
+	private NamedParameterJdbcTemplate jdbcTemplate;
+
+	public List<SIJGM001Dto> getSyainListInfo(SIJGM001Dto dto) {
+
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("SELECT");
+		sb.append("  sd.syain_id");
+		sb.append("  , sd.genba_id");
+		sb.append("  , sd.syain_na");
+		sb.append("  , sd.syain_group_cd");
+		sb.append("  , sd.nyusya_dt");
+		sb.append("  , sd.genba_sankaku_dt");
+		sb.append("  , sd.yakusyoku_cd");
+		sb.append("  , sd.phase_cd");
+		sb.append("  , sd.moyori_eki_1");
+		sb.append("  , sd.moyori_eki_2");
+		sb.append("  , sd.moyori_eki_3");
+		sb.append("  , sd.siyo_rosen_na");
+		sb.append("  , sd.syozoku_team");
+		sb.append("  , sd.birth_dt");
+		sb.append("  , sd.keika_ym");
+		sb.append("  , sd.tanka_vl");
+		sb.append("  , sd.zensyoku_na");
+		sb.append("  , sd.syain_address");
+		sb.append("  , sd.syussin_na");
+		sb.append("  , sd.syain_renrakusaki");
+		sb.append("  , gd.genba_na");
+		sb.append("  , sd.yuko_fg");
+		sb.append("  , sd.sakujo_fg ");
+		sb.append("FROM");
+		sb.append("  syain_dt sd ");
+		sb.append("  LEFT JOIN ( ");
+		sb.append("    SELECT");
+		sb.append("      genba_id");
+		sb.append("      , sakujo_fg");
+		sb.append("      , MAX(genba_na) AS genba_na ");
+		sb.append("    FROM");
+		sb.append("      genba_dt ");
+		sb.append("    GROUP BY");
+		sb.append("      genba_id");
+		sb.append("      , sakujo_fg");
+		sb.append("  ) gd ");
+		sb.append("    ON sd.genba_id = gd.genba_id ");
+		sb.append("    AND gd.sakujo_fg = '0' ");
+		sb.append("WHERE");
+		sb.append("  sd.yuko_fg = '1' ");
+		sb.append("  AND sd.sakujo_fg = '0' ");
+		sb.append("ORDER BY");
+		sb.append("  sd.syain_id");
+
+		return jdbcTemplate.query(sb.toString(),
+				new BeanPropertyRowMapper<SIJGM001Dto>(SIJGM001Dto.class));
+
+	}
+
+	// /**
+	// * userを取得
+	// * @return user
+	// */
+	// //public List<SIJGM001Dto> getUser(SIJGM001Dto SIJGM001Dto);
+	//
+	// /**
+	// * 社員情報一覧を取得
+	// * @param SIJGM001Dto
+	// * @return getSyainListInfo
+	// */
+	// public List<SIJGM001Dto> getSyainListInfo();
 }
